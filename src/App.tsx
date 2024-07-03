753e42ce5ccf28752b397840942125ae5d18d237
Aman123at <ammitiwari1234@gmail.com>
2024-07-03 20:00:42 +0530

    change BrowserRouter to HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react'
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import {HashRouter, Route, Routes} from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import AuthPage from './pages/AuthPage'
 import Dashboard from './pages/Dashboard'
@@ -7,14 +7,14 @@ import Dashboard from './pages/Dashboard'
 const App:FC=()=> {
 
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path='/' element={<HomePage />}  />
         <Route path='/auth' element={<AuthPage />}  />
         <Route path='/dashboard' element={<Dashboard />}  />
         <Route path='*' element={<p>Page Not found</p>}  />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
